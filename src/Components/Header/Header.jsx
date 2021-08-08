a582e693f8846f41d36892d7bed40a24cce7fe35
bftawfik <bftawfik@gmail.com>
2021-08-08 14:20:30 +0200

    add Header title

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,14 @@
+import FulscrnWrpr from "../FulscrnWrpr/FulscrnWrpr";
+
+import classes from "./Header.module.scss";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <FulscrnWrpr className={classes.Header} containerClassName={classes.container}>
+      <div className={classes.row}>
+        <h1 className={classes.title}>Where in the world?</h1>
+      </div>
+    </FulscrnWrpr>
+  );
 };
 export default Header;
