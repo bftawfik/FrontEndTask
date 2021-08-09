0f066b7afdb804bbc12dfa566bc338c9c7c93646
bftawfik <bftawfik@gmail.com>
2021-08-09 10:50:52 +0200

    add cards to home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,25 @@
+import { useSelector } from "react-redux";
+
+import FulscrnWrpr from "../../Components/FulscrnWrpr/FulscrnWrpr";
+import CountryCard from "../../Components/CountryCard/CountryCard";
+
+import classes from "./Home.module.scss";
+
 const Home = () => {
-  return <div>Home</div>;
+  const stateAllCountries = useSelector((state) => state.countries.all);
+  console.log(stateAllCountries);
+
+  return (
+    <FulscrnWrpr
+      className={classes.Home}
+      containerClassName={classes.container}
+    >
+      <div className={classes.cardsContainer}>
+        {stateAllCountries.map((country) => (
+          <CountryCard country={country} />
+        ))}
+      </div>
+    </FulscrnWrpr>
+  );
 };
 export default Home;
